48c87435c17276ff58d3aec9daf46640b29658d9
fastium <fastium.pro@proton.me>
2026-06-05 22:30:02 +0200

    feat(MP/daemon): add oled files and example

---
+++ src/06-mini-project/daemon/oled/oled.c
@@ -0,0 +1,28 @@
+//
+// Created by fastium on 6/5/26.
+//
+
+#include "oled.h"
+
+#include "ssd1306.h"
+
+int display()
+{
+    ssd1306_init();
+
+    ssd1306_set_position (0,0);
+    ssd1306_puts("CSEL1a - SP.07");
+    ssd1306_set_position (0,1);
+    ssd1306_puts("  Demo - SW");
+    ssd1306_set_position (0,2);
+    ssd1306_puts("--------------");
+
+    ssd1306_set_position (0,3);
+    ssd1306_puts("Temp: 35'C");
+    ssd1306_set_position (0,4);
+    ssd1306_puts("Freq: 1Hz");
+    ssd1306_set_position (0,5);
+    ssd1306_puts("Duty: 50%");
+
+    return 0;
+}
---
+++ src/06-mini-project/daemon/oled/oled.h
@@ -0,0 +1,8 @@
+//
+// Created by fastium on 6/5/26.
+//
+
+#ifndef MSE_MA_CSEL_OLED_H
+#define MSE_MA_CSEL_OLED_H
+
+#endif //MSE_MA_CSEL_OLED_H
