9ba7ee11faf6ba7d9fe189dbe2e6ba567cc7e8aa
fastium <fastium.pro@proton.me>
2026-06-05 20:00:10 +0200

    feat(MP/daemon): create ipc server to handle socketpair

---
+++ src/06-mini-project/daemon/ipc/ipc_server.h
@@ -0,0 +1,32 @@
+#ifndef IPC_SERVER_H
+#define IPC_SERVER_H
+
+#include "../../common/common_ipc.h"
+
+/*
+ * Structure holding the callbacks for IPC commands.
+ * Each function pointer will be called when the corresponding command is received.
+ */
+struct ipc_callbacks_t {
+    void (*on_set_mode)(int mode);
+    void (*on_set_frequency)(int freq);
+    void (*on_inc_frequency)(void);
+    void (*on_dec_frequency)(void);
+} ;
+
+/**
+ * start_ipc_server() - Start the IPC background thread.
+ * @cbs: Pointer to the structure containing the callback functions.
+ *
+ * Return: 0 on success, or a negative value on error.
+ */
+int start_ipc_server(struct ipc_callbacks_t *cbs);
+
+/**
+ * stop_ipc_server() - Stop the IPC thread and clean up the socket.
+ *
+ * Return: 0 on success.
+ */
+int stop_ipc_server(void);
+
+#endif //IPC_SERVER_H
